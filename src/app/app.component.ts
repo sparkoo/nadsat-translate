c6c567acaf3d7e2732a8ee8d256eb78b8ae68b07
Michal Vala <michal.vala@gmail.com>
2017-11-03 23:27:43 +0100

    first very naive and ugly implementation with stupid hardcoded dictionary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { phrases } from './Phrase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('english') english: ElementRef;
+  @ViewChild('nadsat') nadsat: ElementRef;
+
+  translateToEnglish() {
+    let text = this.nadsat.nativeElement.value.toLowerCase();
+    for (const phrase of phrases) {
+      for (const nadsatWord of phrase.nadsat) {
+        text = text.split(nadsatWord.toLowerCase()).join(phrase.english[0].toLowerCase());
+      }
+    }
+    this.english.nativeElement.value = text;
+  }
+
+  translateToNadsat() {
+    let text = this.english.nativeElement.value.toLowerCase();
+    for (const phrase of phrases) {
+      for (const englishWord of phrase.english) {
+        text = text.split(englishWord.toLowerCase()).join(phrase.nadsat[0].toLowerCase());
+      }
+    }
+    this.nadsat.nativeElement.value = text;
+  }
 }
